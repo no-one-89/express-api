ec22aa67fb4ff94a3a17e0dcaf51386232fa6fa3
no-one-89 <67049155@qq.com>
2016-09-30 11:47:18 +0800

    add put&etc

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  let page = "<html>"+
-              "<body>"+
-                "<h1>index.html</h1>"+
-              "</body>"+
-            "</html>"
-  res.send(page);
+app.get('/posts', function (req, res) {
+  // let page = "<html>"+
+  //             "<body>"+
+  //               "<h1>index.html</h1>"+
+  //             "</body>"+
+  //           "</html>"
+  res.send('GET /posts');
 });
-app.get('/:name', function (req, res) {
-  let userName = req.params.name
-  let page = "<html>"+
-              "<body>"+
-                "<h1>"+userName+"的购物车"+"</h1>"+
-              "</body>"+
-            "</html>"
-  res.send(page);
+
+app.get('/posts/:id', function (req, res) {
+let userName = req.params.id
+  res.send(`GET /posts/:${userName}`);
 });
-app.post('/:name',function(req,res){
-  res.send(req.params.name)
+
+app.put('/posts/:id',function(req,res){
+  let userName = req.params.id
+
+  console.log(`PUT /posts/:${userName}`);
+})
+
+app.post('/posts',function(req,res){
+  res.send('POST /posts');
+})
+
+app.delete('/posts/:id',function(req,res){
+  res.send('DELETE /posts/:id');
 })
 
 app.listen(3000,function(){
